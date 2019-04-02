66ce15a58730b370bb7deae0cdd562721acd0224
bansalmuskaan <49182620+bansalmuskaan@users.noreply.github.com>
2019-04-02 12:11:40 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,10 +1,22 @@
-#include<stdio.h>
-main()
+#include <stdio.h>
+#include <math.h>
+ 
+void main()
 {
-int i,s=0;
-for(i=0;i=3;i++)
-{
-s=s+i;
-}
-printf("sum=\n",s);
+    int number, sum = 0, rem = 0, cube = 0, temp;
+ 
+    printf ("enter a number");
+    scanf("%d", &number);
+    temp = number;
+    while (number != 0)
+    {
+        rem = number % 10;
+        cube = pow(rem, 3);
+        sum = sum + cube;
+        number = number / 10;
+    }
+    if (sum == temp)
+        printf ("The given no is armstrong no");
+    else
+        printf ("The given no is not a armstrong no");
 }

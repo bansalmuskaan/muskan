d7d5b5d3a2ce266164defe98b4c818b04e9ff4d1
bansalmuskaan <49182620+bansalmuskaan@users.noreply.github.com>
2019-04-16 13:33:12 -0500

    Add files via upload

---
+++ sorted.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 01:52:30 2019
+
+@author: Dell
+"""
+
+#printnumbers=[1,3,4,2]
+#numbers.sort(reverse=True)
+#print(numbers)
+
+#=[2.01,2.00,3.67,3.28,1.68]
+#decimalnumbers.sort()
+#print(decimalnumbers)
+
+#def sortSecond(numbers):
+#    return val[1]
+
+#list1=[('cat','bat'),('sat','cat'),('cat','bat'),('cat','bat','sat'),[1,2],[1,2,3],[1,2]]
+#print(list1.count(('cat','bat')))
+
+#list=[]
+#print(list)
+
+#list=['P','Y','T','H','O','N','F','O','R','G','A','M','E','S']
+#print(list)
+#Sliced_list=list[3:8]
+#print(Sliced_list)
+#Sliced_list=list[::-1]
+#print(Sliced_list)
+
+
+
+
+
+
+
+

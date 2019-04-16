04ace3669cc8e4462663aba36ed92bb9de035719
bansalmuskaan <49182620+bansalmuskaan@users.noreply.github.com>
2019-04-16 13:29:45 -0500

    Add files via upload

---
+++ count.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 00:53:10 2019
+
+@author: Dell
+"""
+
+#list1=[1,2,3,4,1,1,1,4,5]
+#print(list1.index(4,4,8))
+#print(list1.index(1,1,7))
+
+#list2=['cat','bat','mat','cat','get','cat','sat','pet']
+#print(list2.index('cat',2,6))
+
+#list1=[1,2,3,4,1,1,1,4,5]
+#print(list1.index(10))
+ 
+#list1=[1,1,1,2,3,2,1]
+#print(list1.count(1))
+#print(list1.count(1),(2))
+
+#list2=('a','a','a','b','b','a','c','b')
+#print(list2.count('b'))
+
+#list3=['cat','bat','sat','cat','cat','mat']
+#print(list3.count('cat'))
+
+#list3=['cat','bat','sat','cat','cat','mat']
+#print([l,list.count(l)for l in set(list)])
+
